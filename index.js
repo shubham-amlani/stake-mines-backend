24e3cec94c5d2b77d6555b1edc513e0e27e19293
Shubham Amlani <shubhamamlani104@gmail.com>
2024-06-06 05:05:39 +0530

    Issue fix 4

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use('/api', routes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+const HOST = '0.0.0.0';
+app.listen(PORT, HOST, () => {
     console.log(`Server is running on port ${PORT}`);
 });
