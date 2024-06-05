15d094c99bbfd95c953795ff70370de5c7d1a58b
Shubham Amlani <shubhamamlani104@gmail.com>
2024-06-06 05:00:04 +0530

    Issue fix 3

--- config/db.js
+++ config/db.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://admin-shubham:Shubham104@cluster0.2c4ogd3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/stake-mines', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect('mongodb+srv://admin-shubham:Shubham104@cluster0.2c4ogd3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/stake-mines');
     } catch (err) {
         console.error(err.message);
         process.exit(1);
